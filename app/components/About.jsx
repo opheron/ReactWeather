231e4430345b755e3b0ba17282c88d00d840a600
Andrew Chong <opheron@gmail.com>
2017-02-25 19:43:10 -0800

    Style weather and weatherform

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,9 +4,9 @@ var {Link} = require("react-router");
 var About = (props) => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>This app uses <Link to="https://facebook.github.io/react/">React</Link> and the <Link to="https://openweathermap.org/api">OpenWeatherMap API</Link>.</p>
-			<p>Check out the <Link to="https://github.com/opheron/ReactWeather">Github repo</Link>.</p>
+			<h1 className="text-center">About</h1>
+			<p>This app uses <a href="https://facebook.github.io/react/">React</a> and the <a href="https://openweathermap.org/api">OpenWeatherMap API</a>.</p>
+			<p>Check out the <a href="https://github.com/opheron/ReactWeather">Github repo</a>.</p>
 		</div>
 	)
 };
