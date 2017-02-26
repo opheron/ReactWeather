64bc35d1ed08b7ee6371f586d767a2ed80c8f98e
Andrew Chong <opheron@gmail.com>
2017-02-25 19:14:18 -0800

    Style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,12 @@
 var React = require("react");
+var {Link} = require("react-router");
 
 var About = (props) => {
 	return (
 		<div>
 			<h3>About</h3>
-			<p>Welcome to the about page!</p>
+			<p>This app uses <Link to="https://facebook.github.io/react/">React</Link> and the <Link to="https://openweathermap.org/api">OpenWeatherMap API</Link>.</p>
+			<p>Check out the <Link to="https://github.com/opheron/ReactWeather">Github repo</Link>.</p>
 		</div>
 	)
 };
