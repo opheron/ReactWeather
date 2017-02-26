36ec059b947861b9bf7941824bfb1badc65f996d
Andrew Chong <opheron@gmail.com>
2017-02-25 18:03:52 -0800

    Change About page title

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,17 +1,11 @@
 var React = require("react");
 
-// var About = React.createClass({
-// 	render: function() {
-// 		return (
-// 			<h3>About component</h3>
-// 		);
-// 	}
-
-// });
-
 var About = (props) => {
 	return (
-		<h3>About component</h3>
+		<div>
+			<h3>About</h3>
+			<p>Welcome to the about page!</p>
+		</div>
 	)
 };
 
