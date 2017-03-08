15db2685eb65fbcf06abf9b4b48a3cec643a72cf
Andrew Chong <opheron@gmail.com>
2017-03-08 17:56:46 -0500

    Add CSS styles for page title and search

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var {Link} = require("react-router");
 var About = (props) => {
 	return (
 		<div>
-			<h1 className="text-center">About</h1>
+			<h1 className="text-center page-title">About</h1>
 			<p>This app uses <a href="https://facebook.github.io/react/">React</a> and the <a href="https://openweathermap.org/api">OpenWeatherMap API</a>.</p>
 			<p>Check out the <a href="https://github.com/opheron/ReactWeather">Github repo</a>.</p>
 		</div>
